cf98bd94b10027e146f6f40a3233499ee8de03ee
DarkStranded <50366797+DarkStranded@users.noreply.github.com>
2022-09-06 11:14:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,7 +31,7 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "fl",
+      name: "自己",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oDdAE6QIuBJW2QvcG8n-qg-T4sb8",
       // 你想对他发送的模板消息的模板ID
@@ -43,14 +43,14 @@ export const config = {
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://www.xzw.com/fortune/aries/"
     }, 
-//     {
-//       name: "苏苏", 
-//       id: "oDdAE6ezqlVx8CRwOiNnebMWN0MQ",
-//       useTemplateId: "02lHCi8mzPp7BDIofCS05p_WlIywySja_EKGRYk1DNk",
-//       province: "江苏",
-//       city: "苏州",
-//       openUrl: "https://www.xzw.com/fortune/aries/"
-//     }, 
+    {
+      name: "苏苏", 
+      id: "oDdAE6ezqlVx8CRwOiNnebMWN0MQ",
+      useTemplateId: "02lHCi8mzPp7BDIofCS05p_WlIywySja_EKGRYk1DNk",
+      province: "江苏",
+      city: "苏州",
+      openUrl: "https://www.xzw.com/fortune/aries/"
+    }, 
   ],
 
   /**
