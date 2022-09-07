eff1d444e112e1f40eb1a800c1f0f267d1169862
DarkStranded <50366797+DarkStranded@users.noreply.github.com>
2022-09-07 08:05:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -43,14 +43,14 @@ export const config = {
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://www.xzw.com/fortune/aries/"
     }, 
-//     {
-//       name: "苏苏", 
-//       id: "oDdAE6ezqlVx8CRwOiNnebMWN0MQ",
-//       useTemplateId: "FXwMDCahG5FgT40KD56eQvyDqIk47MpJK5cp0LuQj6E",
-//       province: "江苏",
-//       city: "苏州",
-//       openUrl: "https://www.xzw.com/fortune/aries/"
-//     }, 
+    {
+      name: "苏苏", 
+      id: "oDdAE6ezqlVx8CRwOiNnebMWN0MQ",
+      useTemplateId: "FXwMDCahG5FgT40KD56eQvyDqIk47MpJK5cp0LuQj6E",
+      province: "江苏",
+      city: "苏州",
+      openUrl: "https://www.xzw.com/fortune/aries/"
+    }, 
   ],
 
   /**
